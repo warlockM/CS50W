e80fdc733b556a81846c654a2d803780b8aaf902
warlockM <32521710+warlockM@users.noreply.github.com>
2020-07-13 12:40:52 +0530

    Adding file feature added

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("search/", views.search, name="search"),
-    path("", views.index, name="index")
+    path("save/", views.save, name = "save"), #this route is used to save data as md file and return to index.html
+    path("create/", views.create, name="create"), #this route is used to go to the form where title and description are entered for new entry.
+    path("search/", views.search, name="search"), #this route is used to search for any entry in the app.
+    path("", views.index, name="index") #this route is used to visit the index
 ]
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from . import util
-
+"""
+Variables
+==============================================================================================================
+1. title is global varaible used to store the search query passed from the form in layout.html
+2. new_title is global variable used to store the title of new data to be added passed from create.html
+3. new_desc is global variable used to store the description of new data to be added passed from create.html
+==============================================================================================================
+Functions
+==============================================================================================================
+1. index function is used to display all the data that exists in entries folder as .md file.
+2. search function is used to search the wiki page for existing data.
+3. create function is used to visit the form page for entrering new data.
+4. save function is used to save the files as .md files with title as file name and description as the data in the .md file
+"""
 title = ""
+new_title = ""
+new_desc = ""
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -17,4 +32,18 @@ def search(request):
         return HttpResponse("Resubmit form!")
     return render(request, "encyclopedia/get.html", {
         "data": util.get_entry(title), "header": title
+    })
+
+def create(request):
+    return render(request, "encyclopedia/create.html")
+
+def save(request):
+    if request.method == "POST":
+        new_title = request.POST.get("t")
+        new_desc = request.POST.get("d")
+
+    else:
+        return HttpResponse("Data not created!")
+    return render(request, "encyclopedia/index.html", {
+        "new_data": util.save_entry(new_title, new_desc)
     })
